690c5d197ff22c154740008391bfd85895e2f3f9
GauravTandi <148497273+GauravTandi@users.noreply.github.com>
2026-06-06 14:14:53 +0000

    add pdf text extraction and chunking

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
-
+from PyPDF2 import PdfReader
+import io
 app = FastAPI()
 
 @app.get("/")
@@ -11,7 +12,27 @@ def home():
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
     content = await file.read()
+    pdf_file = io.BytesIO(content)
+    reader = PdfReader(pdf_file)
+
+    text = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text: #text += page_text or ""
+            text += page_text
+    
+    def chunk(text, chunk_size):
+        chunks = []
+        for i in range(0, len(text), chunk_size):
+            chunks.append(text[i : i + chunk_size ])
+        return chunks
+    chunk_size = 300
+    chunks = chunk(text,chunk_size)
+    
     return {
         "filename":file.filename,
-        "size":len(content)
+        "size":len(content),
+        "preview_file": chunks[:1],
+        "total_chunks": len(chunks),
+        "first_chunk": chunks[0]
     }
